4f76d33c9c5dc5a73081ed9165a47875aa483d06
Chris Kerr <chriskerr@me.com>
2021-12-03 21:30:44 +1100

    updated typings and bumped package

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,3 @@
-export default function deepTypeEquals<T>(referenceInput: T, dataToTest: T): boolean;
+export default function deepTypeEquals<T>(referenceInput: T, dataToTest: Omit<T, "">): boolean;
 export declare function union<T>(...args: T[]): T;
 export declare function unknown(): unknown;
